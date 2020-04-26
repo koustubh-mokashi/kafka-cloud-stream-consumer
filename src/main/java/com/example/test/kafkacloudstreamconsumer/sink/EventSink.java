681b45b51f3e35b4bac6d96c2169ef0594b550a5
Koustubh Mokashi <koustubhmokashi@Koustubhs-MacBook.local>
2020-04-26 21:57:46 +0530

    Added custom Sink

--- src/main/java/com/example/test/kafkacloudstreamconsumer/sink/EventSink.java
+++ src/main/java/com/example/test/kafkacloudstreamconsumer/sink/EventSink.java
@@ -1,10 +1,9 @@
 package com.example.test.kafkacloudstreamconsumer.sink;
 
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.SubscribableChannel;
 
-public interface EventSink extends Sink {
+public interface EventSink {
 
 	/**
 	 * Input channel name.
